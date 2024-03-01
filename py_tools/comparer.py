b652e5a9f0e40566b71da6bd1c1286142c5c0ce6
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-03-01 06:47:16 +0300

    feat(comparer): remove N

--- py_tools/comparer.py
+++ py_tools/comparer.py
@@ -1,18 +1,17 @@
 import numpy as np
 
 path_a = "result_py.txt"
-# path_b = "C:/Users/kotyr/CLionProjects/it_math_1/cmake-build-debug/result.txt"
-path_b = "C:/Users/kotyr/Downloads/LENA_FINAL_BREATH (4).txt"
+path_b = "C:/Users/kotyr/CLionProjects/it_math_1/cmake-build-debug/result.txt"
+# path_b = "C:/Users/kotyr/Downloads/LENA_FINAL_BREATH (9).txt"
 delta = 0.5
-N = 100
 
 matrix_a = np.loadtxt(path_a)
 matrix_b = np.loadtxt(path_b)
 
 max_delta = 0
 
-for i in range(N+2):
-    for j in range(N+2):
+for i in range(len(matrix_a)):
+    for j in range(len(matrix_a[0])):
         cur_delta = abs(matrix_b[i][j] - matrix_a[i][j])
         if cur_delta > delta:
             print(f"pizdec in position {i},{j}: in a = {matrix_a[i][j]}, in b = {matrix_b[i][j]}")
