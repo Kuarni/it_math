d3e1c9f7df172ec9a8356cd333f7e81dbe6ac79a
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-03-10 01:03:53 +0300

    feat(it_math_1): add additional data to max_delta in comparer

--- it_math_1/py_tools/comparer.py
+++ it_math_1/py_tools/comparer.py
@@ -8,14 +8,18 @@ delta = 0.5
 matrix_a = np.loadtxt(path_a)
 matrix_b = np.loadtxt(path_b)
 
-max_delta = 0
+max_delta = [0, (0, 0), 0, 0]
 
 for i in range(len(matrix_a)):
     for j in range(len(matrix_a[0])):
         cur_delta = abs(matrix_b[i][j] - matrix_a[i][j])
         if cur_delta > delta:
             print(f"pizdec in position {i},{j}: in a = {matrix_a[i][j]}, in b = {matrix_b[i][j]}")
-        if cur_delta > max_delta:
-            max_delta = cur_delta
+        if cur_delta > max_delta[0]:
+            max_delta[0] = cur_delta
+            max_delta[1] = (i, j)
+            max_delta[2] = matrix_a[i][j]
+            max_delta[3] = matrix_b[i][j]
 
-print(f"max_delta = {max_delta}")
+print(
+    f"max_delta = {max_delta[0]}, position = ({max_delta[1][0]},{max_delta[1][0]}), value = {max_delta[2]}, {max_delta[3]}")
